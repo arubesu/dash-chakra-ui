4c35c7fab181c864ce32b481c4f78b245c1680b3
Bruno Souza <bruno.arubesu@gmail.com>
2021-06-19 16:27:41 -0300

    feat(dashboard): add responsive layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -85,7 +85,7 @@ export default function Dashboard() {
           minChildWidth='320px'
           align='flex-start'>
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
@@ -94,7 +94,7 @@ export default function Dashboard() {
             <Chart type='area' options={options} series={series} height={160} />
           </Box>
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
