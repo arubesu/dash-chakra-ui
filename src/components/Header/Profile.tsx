f5f932af9057c2fc1ad609dd947fbce3a24b0c38
Bruno Souza <bruno.arubesu@gmail.com>
2021-06-19 13:04:17 -0300

    feat(header): add responsive layout

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text> Bruno Souza</Text>
-        <Text color='gray.300'
-          fontSize='small'>bruno.arubesu@gmail.com</Text>
-      </Box>
-
+      {
+        showProfileData && (
+          <Box mr='4' textAlign='right'>
+            <Text> Bruno Souza</Text>
+            <Text color='gray.300'
+              fontSize='small'>bruno.arubesu@gmail.com</Text>
+          </Box>
+        )
+      }
       <Avatar
         size='md'
         name='Bruno Souza'
