a3789adc4498ea66518393a233b01997e31973f9
Bruno Souza <bruno.arubesu@gmail.com>
2021-06-20 19:06:48 -0300

    feat(sign in): add form and validation

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -3,34 +3,46 @@ import {
   FormLabel,
   InputProps as ChakraInputProps,
   Input as ChakraInput,
+  FormErrorMessage,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+const InputForwardRef: ForwardRefRenderFunction<HTMLInputElement, InputProps> =
+  ({ name, label, error = null, ...rest }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
-      <ChakraInput
-        name={name}
-        id={name}
-        focusBorderColor='gray.900'
-        borderColor='gray.900'
-        bg='gray.900'
-        variant='filled'
-        _hover={{
-          bgColor:
-            'gray.900'
-        }}
-        size='lg'
-        {...rest}
-      >
+        <ChakraInput
+          name={name}
+          id={name}
+          focusBorderColor='gray.900'
+          borderColor='gray.900'
+          bg='gray.900'
+          variant='filled'
+          _hover={{
+            bgColor:
+              'gray.900'
+          }}
+          size='lg'
+          {...rest}
+          ref={ref}
+        />
 
-      </ChakraInput>
-    </FormControl>
-  )
-}
+        {!!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
+      </FormControl>
+    )
+  }
+
+export const Input = forwardRef(InputForwardRef)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,41 @@
 import { Stack } from '@chakra-ui/react'
 import { Flex, Button } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+interface FormInput {
+  email: string;
+  password: string;
+}
+
+type SignInFormData = FormInput;
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8, 'Password is too short - should be 8 chars minimum.'),
+});
 
 export default function Home() {
+  const {
+    handleSubmit,
+    register,
+    formState: {
+      errors,
+      isSubmitting
+    }
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 1000)
+    })
+    console.log(values);
+  }
+
   return (
     <Flex
       w='100vw'
@@ -18,6 +51,7 @@ export default function Home() {
         p='8'
         borderRadius={8}
         flexDir='column'
+        onSubmit={handleSubmit(handleSignIn)}
       >
 
         <Stack spacing='4'>
@@ -25,12 +59,18 @@ export default function Home() {
           <Input
             name='email'
             type='email'
-            label='E-mail' />
+            label='E-mail'
+            error={errors.email}
+            {...register('email')}
+          />
 
           <Input
             name='password'
             type='password'
-            label='Password' />
+            label='Password'
+            error={errors.password}
+            {...register('password')}
+          />
 
         </Stack>
 
@@ -38,7 +78,9 @@ export default function Home() {
           type='submit'
           mt='6'
           colorScheme='pink'
-          size='lg'>
+          size='lg'
+          isLoading={isSubmitting}
+        >
           Sign In
         </Button>
 
